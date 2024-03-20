548772f6a043d42e6b4619a703a0fb85f4e9c915
Ayberk Keskin <81094165+Ayberkeskin@users.noreply.github.com>
2024-03-20 18:19:07 +0300

    jump(hata var)

--- Assets/Scripts/Character/PlayerMovement.cs
+++ Assets/Scripts/Character/PlayerMovement.cs
@@ -1,65 +1,91 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using JetBrains.Annotations;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-   [SerializeField] private Rigidbody _rb;
-   [SerializeField] private float _speed;
+    [SerializeField] private Rigidbody _rb;
+    [SerializeField] private Transform _groundCheck;
+    [SerializeField] private LayerMask _groundLayer;
+    [SerializeField] private float _speed;
+    [SerializeField] private float _jumpForce;
 
-   [SerializeField] private Transform _leftTransform;
-   [SerializeField] private Transform _midTransform;
-   [SerializeField] private Transform _rightTransform;
+    [SerializeField] private Transform _leftTransform;
+    [SerializeField] private Transform _midTransform;
+    [SerializeField] private Transform _rightTransform;
 
-   private Transform _currentTransform;
-   private Vector3 _targetPosition;
+    private Transform _currentTransform;
+    private Vector3 _targetPosition;
+    private bool _isJumping = false;
 
-   private void Awake()
-   {
-      gameObject.transform.position = _midTransform.position;
-   }
+    private void Awake()
+    {
+        gameObject.transform.position = _midTransform.position;
+    }
 
-   private void Start()
-   {
-      _currentTransform = _midTransform;
-      _targetPosition = _midTransform.position; 
-   }
+    private void Start()
+    {
+        _currentTransform = _midTransform;
+        _targetPosition = _midTransform.position;
+    }
 
-   private void Update()
-   {
-      Move();
-      transform.position = Vector3.Lerp(transform.position, _targetPosition, Time.deltaTime * _speed);
-   }
+    private void Update()
+    {
+        Debug.Log(IsGrounded());
+        Move();
+        if (!_isJumping)
+        {
+            transform.position = Vector3.Lerp(transform.position, _targetPosition, Time.deltaTime * _speed);
+        }
+        if (Input.GetKeyDown(KeyCode.Space)&&IsGrounded())
+        {
+            Jump();
+        }
+    }
 
-   private void Move()
-   {
-      if (Input.GetKeyDown(KeyCode.A))
-      {
-         if (_currentTransform == _rightTransform)
-         {
-            _currentTransform = _midTransform;
-            _targetPosition = _midTransform.position; 
-         }
-         else if (_currentTransform == _midTransform)
-         {
-            _currentTransform = _leftTransform;
-            _targetPosition = _leftTransform.position; 
-         }
-      }
-      else if (Input.GetKeyDown(KeyCode.D))
-      {
-         if (_currentTransform == _leftTransform)
-         {
-            _currentTransform = _midTransform;
-            _targetPosition = _midTransform.position; 
-         }
-         else if (_currentTransform == _midTransform)
-         {
-            _currentTransform = _rightTransform;
-            _targetPosition = _rightTransform.position; 
-         }
-      }
-   }
+    
+    private void Move()
+    {
+        if (!_isJumping && (Input.GetKeyDown(KeyCode.A)))
+        {
+            if (_currentTransform == _rightTransform)
+            {
+                _currentTransform = _midTransform;
+                _targetPosition = _midTransform.position;
+            }
+            else if (_currentTransform == _midTransform)
+            {
+                _currentTransform = _leftTransform;
+                _targetPosition = _leftTransform.position;
+            }
+        }
+        else if (!_isJumping && (Input.GetKeyDown(KeyCode.D)))
+        {
+            if (_currentTransform == _leftTransform)
+            {
+                _currentTransform = _midTransform;
+                _targetPosition = _midTransform.position;
+            }
+            else if (_currentTransform == _midTransform)
+            {
+                _currentTransform = _rightTransform;
+                _targetPosition = _rightTransform.position;
+            }
+        }
+    }
+
+    private void Jump()
+    {
+        _isJumping = true;
+        _rb.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+        Invoke("ResetJump", 1.0f); 
+    }
+
+    
+    private void ResetJump()
+    {
+        _isJumping = false;
+    }
+    private bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, 0.1f, _groundLayer);
+    }
 }
